1ac2ab70ea36284425d1aa22b3076b032a655b8d
Robert Lego <robertlego@gmail.com>
2018-02-03 01:09:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function getFirstSelector(selector) {
   const lis = document.getElementById('app').querySelectorAll(selector);
   return lis[0];
 }
+
+function nestedTarget() {
+  return document.querySelector('div');
+}
