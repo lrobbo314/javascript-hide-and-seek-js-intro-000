f06d31b9618d9f28d29f10062f5f9d2b6e0deff0
Robert Lego <robertlego@gmail.com>
2018-02-03 01:25:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i + 1).parseInt();
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) +1;
   }
 }
